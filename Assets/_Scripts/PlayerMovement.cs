9e76b075e62c5640e776bd4f67f6c243803b0605
kleich <leichkay@gmail.com>
2022-11-06 17:41:37 -0500

    Add player movement animations

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float _moveSpeed = 2f;
-    private Vector2 _moveInput;
+    [SerializeField] private Vector2 _moveInput;
     private Rigidbody2D _rb;
     public bool InEncounterArea { get { return _inEncounterArea; } set { _inEncounterArea = value; } }
     private bool _inEncounterArea = false;
@@ -19,9 +19,13 @@ public class PlayerMovement : MonoBehaviour
     public float EncounterAreaMovementTimer { get { return _encounterAreaMovementTimer; } set { _encounterAreaMovementTimer = value; } }
     private float _encounterAreaMovementTimer = 0f;
     private Vector2 _startPosition = new Vector2(4.3f, -1.5f);
+    
+    private Animator _animator;
+    private int _moveDirection; // 0 = Up, 2 = Down, 1 = Left/Right 
 
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
         SetPlayerStartPosition();
     }
@@ -30,7 +34,10 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         if(!_inEncounter)
+        {
             Move();
+            ConfigureAnimation();
+        }
     }
 
     private void Move()
@@ -62,5 +69,30 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void ConfigureAnimation()
+    {
+        _animator.SetFloat("MoveSpeed", (_moveInput * _moveSpeed).magnitude);
+
+        if(_moveInput.y > 0) // Moving Up
+        {
+            _moveDirection = 0;
+        }
+        if (_moveInput.y < 0) // Moving Down
+        {
+            _moveDirection = 2;
+        }
+        if(_moveInput.x > 0) // Moving Right
+        {
+            _moveDirection = 1;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if(_moveInput.x < 0) // Moving Left
+        {
+            _moveDirection = 1;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        _animator.SetInteger("Direction", _moveDirection);
+    }
 }
 
